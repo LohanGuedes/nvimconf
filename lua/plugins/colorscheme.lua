fb2a75c5f8b8ade0046c9d61c49f2950ab457a60
Lguedes <lohan.guedes@nutrix.tech>
2023-11-29 15:54:09 -0300

    Remove added by hand extras, and some dead configs

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,19 +1,19 @@
-return {
-  { "catppuccin/nvim", name = "catppuccin" },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    }
-  }
-}
-
 -- return {
---   { "ellisonleao/gruvbox.nvim"},
+--   { "catppuccin/nvim", name = "catppuccin" },
 --   {
 --     "LazyVim/LazyVim",
 --     opts = {
---       colorscheme = "gruvbox",
---     },
---   },
+--       colorscheme = "catppuccin",
+--     }
+--   }
 -- }
+
+return {
+  { "ellisonleao/gruvbox.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
